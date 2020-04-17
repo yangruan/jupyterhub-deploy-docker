9a3b49794cc59ff8b0faf7d66458d5933f8d8c5b
Yang Ruan <yang.ruan@truvaluelabs.com>
2020-04-16 21:13:53 -0700

    try jupyter lab command

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -20,9 +20,7 @@ c.DockerSpawner.container_image = os.environ['DOCKER_NOTEBOOK_IMAGE']
 # spawning containers.  Optionally, you can override the Docker run command
 # using the DOCKER_SPAWN_CMD environment variable.
 spawn_cmd = os.environ.get('DOCKER_SPAWN_CMD') #, "start-singleuser.sh")
-spawn_cmd = "start-singleuser.sh"
 c.DockerSpawner.extra_create_kwargs.update({ 'command': spawn_cmd})
-c.DockerSpawner.environment = {'GRANT_SUDO' : '1'}
 # Connect containers to this Docker network
 network_name = os.environ['DOCKER_NETWORK_NAME']
 c.DockerSpawner.use_internal_ip = True
