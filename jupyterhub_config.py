d1a295ab6c843aadfcf49c91826c16db56766d8d
Yang Ruan <yang.ruan@truvaluelabs.com>
2020-04-16 20:09:46 -0700

    more options to grant sudo

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -21,8 +21,8 @@ c.DockerSpawner.container_image = os.environ['DOCKER_NOTEBOOK_IMAGE']
 # using the DOCKER_SPAWN_CMD environment variable.
 spawn_cmd = os.environ.get('DOCKER_SPAWN_CMD') #, "start-singleuser.sh")
 spawn_cmd = "start-singleuser.sh"
-c.DockerSpawner.extra_create_kwargs.update({ 'command': spawn_cmd })
-c.DockerSpawner.environment = {'GRANT_SUDO' : 'yes'}
+c.DockerSpawner.extra_create_kwargs.update({ 'command': spawn_cmd, 'user': 'root'})
+c.DockerSpawner.environment = {'GRANT_SUDO' : '1', 'UID': '0'}
 # Connect containers to this Docker network
 network_name = os.environ['DOCKER_NETWORK_NAME']
 c.DockerSpawner.use_internal_ip = True
